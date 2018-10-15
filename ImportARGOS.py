695cf012c1fda27e29151084b5e35be011ff3054
ywjong <yj107@duke.edu>
2018-10-15 12:35:47 -0400

    Gets TagID attribute

--- ImportARGOS.py
+++ ImportARGOS.py
@@ -16,3 +16,26 @@ import sys, os, arcpy
 # Set input variables (Hard-wired)
 inputFile = 'V:/ARGOSTracking/Data/ARGOSData/1997dg.txt'
 outputFC = "V:/ARGOSTracking/Scratch/ARGOStrack.shp"
+
+#Open the ARGOS data file for reading
+inputFileObj = open(inputFile, 'r')
+
+#Get the first line so we can loop
+lineString = inputFileObj.readline()
+print(lineString)
+
+while lineString:
+    #set code to run only if the line contains "Date :"
+    if "Date :" in lineString:
+        #Split the line string into a list
+        lineList = lineString.split()#split by space
+        print(lineList)
+
+        #Get attributes from first line
+        tagID = lineList[0]
+        print(tagID)
+        #print(lineString)
+        break #So it doesnt keep running
+    
+    #Get the next line
+    lineString = inputFileObj.readline()
